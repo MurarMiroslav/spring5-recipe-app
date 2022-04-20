3418d3200eae7abe9b088a6f9bac0583c7f0ba24
Miroslav <mmurar@scoop-software.cz>
2022-04-20 14:49:00 +0200

    Adding OneToOne Bidirection (each Entity has OneToOne annotation to attribute of opposite

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -2,10 +2,12 @@ package guru.springframework.domain;
 
 import java.math.BigDecimal;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Ingredient {
@@ -14,10 +16,12 @@ public class Ingredient {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-
 	private String description;
 	private BigDecimal amount;
 
 	@ManyToOne
 	private Recipe recipe;
+
+	@OneToOne(fetch = FetchType.EAGER)
+	private UnitOfMeasure unitOfMeasure;
 }
