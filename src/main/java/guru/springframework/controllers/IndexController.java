ddfe9a552ec3ae7c6c193631c7769a03b5103140
Miroslav <mmurar@scoop-software.cz>
2022-04-21 16:56:10 +0200

    Assignment, have a look at AplicationListener if I am correct it means that this happens on start up

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -4,30 +4,25 @@ import guru.springframework.domain.Category;
 import guru.springframework.domain.UnitOfMeasure;
 import guru.springframework.repositories.CategoryRepository;
 import guru.springframework.repositories.UnitOfMeasureRepository;
+import guru.springframework.service.RecipeService;
 import java.util.Optional;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
-	private final CategoryRepository categoryRepository;
-	private final UnitOfMeasureRepository unitOfMeasureRepository;
+	private final RecipeService recipeService;
 
-	public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-		this.categoryRepository = categoryRepository;
-		this.unitOfMeasureRepository = unitOfMeasureRepository;
+	public IndexController(RecipeService recipeService) {
+		this.recipeService = recipeService;
 	}
 
 	@RequestMapping({"", "/", "index"})
-	public String getIndexPage() {
-		System.out.println("Some message to say... 13ss");
+	public String getIndexPage(Model model) {
 
-		Optional<Category> category = categoryRepository.findByDescription("American");
-		Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
-
-		System.out.println("Category Id is: " + category.get().getId());
-		System.out.println("UniteMeasure Id is: " + unitOfMeasure.get().getId());
+		model.addAttribute("recipes", recipeService.getRecipies());
 
 		return "index";
 	}
