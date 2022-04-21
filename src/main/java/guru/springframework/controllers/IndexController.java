d1202a46790d848ffd279c56abeae3cb88c49871
Miroslav <mmurar@scoop-software.cz>
2022-04-21 16:15:15 +0200

    Example of generation SQL statement from name of method

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,14 +1,36 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
+import java.util.Optional;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
+	private final CategoryRepository categoryRepository;
+	private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+	public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+		this.categoryRepository = categoryRepository;
+		this.unitOfMeasureRepository = unitOfMeasureRepository;
+	}
+
 	@RequestMapping({"", "/", "index"})
 	public String getIndexPage() {
 		System.out.println("Some message to say... 13ss");
+
+		Optional<Category> category = categoryRepository.findByDescription("American");
+		Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+		System.out.println("Category Id is: " + category.get().getId());
+		System.out.println("UniteMeasure Id is: " + unitOfMeasure.get().getId());
+
 		return "index";
 	}
+
+
 }
