14e9d568b20b33bf0275e9532c31f5bfbbc4c50e
Miroslav <mmurar@scoop-software.cz>
2022-06-01 20:58:52 +0200

    Refractoring tests but we gets some warning which is not getting in course, anyway tests passed...

--- src/test/java/guru/springframework/services/RecipeServiceIT.java
+++ src/test/java/guru/springframework/services/RecipeServiceIT.java
@@ -9,7 +9,6 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,7 +19,7 @@ import static org.junit.Assert.assertEquals;
 /**
  * Created by jt on 6/21/17.
  */
-//@DataMongoTest
+@Ignore
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RecipeServiceIT {
@@ -39,7 +38,7 @@ public class RecipeServiceIT {
     @Autowired
     RecipeToRecipeCommand recipeToRecipeCommand;
 
-  //  @Transactional
+    @Transactional
     @Test
     public void testSaveOfDescription() throws Exception {
         //given
